9d315876e4713b9bda444dab6f0c73f1314e240d
Yuriy Brovko <ysb.ptc@gmail.com>
2022-08-31 13:45:00 +0300

    update file

--- src/components/Counter/Value/index.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import style from './style.module.css';
-
-const Value = ({ value }) => (
-  <span className={style.Сounter__value}>{value}</span>
-);
-
-export default Value;
