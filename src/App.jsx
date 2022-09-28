3a42e353862ac34a7117d6847eef333603f9a264
Yuriy Brovko <ysb.ptc@gmail.com>
2022-09-28 12:06:17 +0300

    training 28.09

--- src/App.jsx
+++ src/App.jsx
@@ -48,35 +48,35 @@
 
 import { useState, useEffect } from 'react';
 
-// const App = () => {
-//   const [value, setValue] = useState(0);
-
-//   useEffect(() => {
-//     document.title = `You clicked ${value} times`;
-//   });
-
-//   return (
-//     <div>
-//       <p>You clicked {value} times</p>
-//       <button onClick={() => setValue(value + 1)}>Click me</button>
-//     </div>
-//   );
-// };
-
 const App = () => {
-  const [value] = useState(0);
-  const [firstValue, setFirstValue] = useState(0);
-  const [secondValue, setSecondValue] = useState(0);
+  const [value, setValue] = useState(0);
 
   useEffect(() => {
-    console.log(firstValue + secondValue);
-  }, [firstValue, secondValue]);
+    document.title = `You clicked ${value} times`;
+  });
 
   return (
-    <>
-      <button onClick={() => setFirstValue(value + 1)}>First: {value}</button>
-      <button onClick={() => setSecondValue(value + 1)}>Second: {value}</button>
-    </>
+    <div>
+      <p>You clicked {value} times</p>
+      <button onClick={() => setValue(value + 1)}>Click me</button>
+    </div>
   );
 };
+
+// const App = () => {
+//   const [value] = useState(0);
+//   const [firstValue, setFirstValue] = useState(0);
+//   const [secondValue, setSecondValue] = useState(0);
+
+//   useEffect(() => {
+//     console.log(firstValue + secondValue);
+//   }, [firstValue, secondValue]);
+
+//   return (
+//     <>
+//       <button onClick={() => setFirstValue(value + 1)}>First: {value}</button>
+//       <button onClick={() => setSecondValue(value + 1)}>Second: {value}</button>
+//     </>
+//   );
+// };
 export default App;
